89bd5b9aff524efc8bce1eae0801762bff6fbf58
Jet Lee <pupboss.i@gmail.com>
2016-09-12 18:04:14 +0800

    Initial Commit

---
+++ BayCarouselView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BayCarouselView
+//
+//  Created by Jet Lee on 9/12/16.
+//  Copyright © 2016 Shanbay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BayCarouselView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BayCarouselView
+//
+//  Created by Jet Lee on 9/12/16.
+//  Copyright © 2016 Shanbay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
